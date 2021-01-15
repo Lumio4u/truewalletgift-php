001a22c7fe7d3bfea6171fc77f1ed879fddafbcc
Moonlight <19193186+Mixzis@users.noreply.github.com>
2021-01-15 07:41:39 +0700

    Update rb_tw.class.php

--- rb_tw.class.php
+++ rb_tw.class.php
@@ -1,7 +1,6 @@
 <?php
 /*
 By Moonlight#6666
-Website : https://rebirth-rc.com/
 Original : https://github.com/MinecraftChannel/VoucherTopup
 
 
@@ -31,7 +30,7 @@ class rb_gift {
 	function RedeemVoucher($hash = null,$phone = null) {
         if (is_null($hash) || is_null($phone)) return false;
         $ch = curl_init();
-		$hash = explode('?v=',$hash)[1];
+	$hash = explode('?v=',$hash)[1];
         $headers  = [
             'Content-Type: application/json',
             'Accept: application/json'
